dd3cc246d1ed63b0f75baa66fbe0e5950bfd7c24
Enochneier <enochneier@gmail.com>
2023-07-23 20:35:26 -0700

    C - Simple Shell

---
+++ change_dir.c
@@ -0,0 +1,20 @@
+#include "simple_shell.h"
+/**
+  * change_dir - Afunction that changes working directory.
+  * @path: The new current working directory.
+  * Return: 0 on success, -1 on failure.
+  */
+int change_dir(const char *path)
+{
+	char *buf = NULL;
+	size_t size = 1024;
+
+	if (path == NULL)
+		path = getcwd(buf, size);
+	if (chdir(path) == -1)
+	{
+		perror(path);
+		return (98);
+	}
+	return (1);
+}
---
+++ shell_init.c
@@ -0,0 +1,110 @@
+#include "simple_shell.h"
+
+/**
+ * main - function that runs our shell.
+ * @ac: The number of inputed arguments.
+ * @av: The pointer to array of inputed arguments.
+ * @env: The pointer to array of enviromental variables.
+ * Return: Always 0.
+ */
+int main(int ac, char **av, char **env)
+{
+	char *buffer = NULL, **command = NULL;
+	size_t buf_size = 0;
+	ssize_t chars_readed = 0;
+	int cicles = 0;
+	(void)ac;
+
+	while (1)
+	{
+		cicles++;
+		prompt();
+		signal(SIGINT, handle);
+		chars_readed = getline(&buffer, &buf_size, stdin);
+		if (chars_readed == EOF)
+			_EOF(buffer);
+		else if (*buffer == '\n')
+			free(buffer);
+		else
+		{
+			buffer[_strlen(buffer) - 1] = '\0';
+			command = tokening(buffer, " \0");
+			free(buffer);
+			if (_strcmp(command[0], "exit") != 0)
+				shell_exit(command);
+			else if (_strcmp(command[0], "cd") != 0)
+				change_dir(command[1]);
+			else
+				create_child(command, av[0], env, cicles);
+		}
+		fflush(stdin);
+		buffer = NULL, buf_size = 0;
+	}
+	if (chars_readed == -1)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+
+/**
+ * prompt - function that prints the prompt
+ * Return: 0
+ */
+void prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "Hell_Shell>> ", 13);
+}
+
+
+/**
+ * handle - function to handle Ctr + C signal.
+ * @signals: The signal to handle.
+ * Return: 0
+ */
+void handle(int signals)
+{
+	(void)signals;
+	write(STDOUT_FILENO, "\nHell_Shell>> ", 14);
+}
+
+
+/**
+ * _EOF - function that chaecks if buffer is EOF
+ * @buffer: The pointer to the input string.
+ * Return: 0
+ */
+void _EOF(char *buffer)
+{
+	if (buffer)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+
+	if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "\n", 1);
+	free(buffer);
+	exit(EXIT_SUCCESS);
+}
+
+
+/**
+ * shell_exit - function that exits the shell.
+ * @command: The pointer to tokenized command.
+ * Return: 0
+ */
+void shell_exit(char **command)
+{
+	int sta_tus = 0;
+
+	if (command[1] == NULL)
+	{
+		free_dp(command);
+		exit(EXIT_SUCCESS);
+	}
+
+	sta_tus = _atoi(command[1]);
+	free_dp(command);
+	exit(sta_tus);
+}
